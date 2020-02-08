1313ed54b6d22e7ae5687b1c79a29b0cc89f17ab
Yexin Tian <xintianye@outlook.com>
2020-02-08 00:29:47 -0500

    init commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+
+/*
+    app: Express App as route handler
+    .get: watch for incoming requests with this method
+    req: incoming request object
+    res: outgoing response object
+    res.send({ hi: 'there' }): immediately send some JSON back to who made this request
+ */
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
